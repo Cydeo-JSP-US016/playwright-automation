bfb8b08419e4bf505feb4443fe6cea812da077d4
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2026-05-17 12:48:01 -0400

    Playwright automation

--- tests/reviews/start-application-page.spec.js
+++ tests/reviews/start-application-page.spec.js
@@ -1,37 +1,55 @@
+import { test, expect } from "@playwright/test";
 
-import { test } from '@playwright/test';
+test.describe("Start Application Step", () => {
+  test.beforeEach(async ({ page }) => {
+    let encodedCredentials = Buffer.from(
+      `${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`,
+    ).toString("base64"); // token ....
 
-test.describe('Start Application Step', () => {
+    await page.setExtraHTTPHeaders({
+      Authorization: `Basic ${encodedCredentials}`,
+    });
 
-    test.beforeEach(async ({ page }) => {
+    await page.goto(process.env.SEP_QA_URL);
+    await page.waitForTimeout(3000);
+  });
 
-        let encodedCredentials = Buffer.from(`${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`).toString('base64'); // token ....
+  test("Verify that clicking the Terms & Conditions link opens a new Terms & Conditions tab", async ({
+    page,
+  }) => {
+    let windowPopupEventListener = page.waitForEvent("popup");
 
-        await page.setExtraHTTPHeaders({
-            'Authorization': `Basic ${encodedCredentials}`
-        });
+    let termsConditionLink = page.getByRole("link", {
+      name: "Terms and conditions",
+    });
+    //a[text()='Terms and conditions']
+    await expect(termsConditionLink).toBeVisible();
+    await expect(termsConditionLink).toBeEnabled();
 
-        await page.goto(process.env.SEP_QA_URL);
-        await page.waitForTimeout(3000);
-    })
+    await termsConditionLink.click(); // triggers pop up window
+    let page2 = await windowPopupEventListener;
+
+    let termsConditionsHeading = page2.getByRole('heading', { name: 'Terms and Conditions' });
+
+    await expect(termsConditionsHeading).toBeVisible();
     
-
-
-  test('Verify that clicking the Terms & Conditions link opens a new Terms & Conditions tab', async ({ page }) => {
-     page.waitForEvent("popup");
-    
   });
 
 
-  test('Verify that the first stepper is blue initially and changes to green once Step 1 is completed', async ({ page }) => {
 
-  });
+  test("Verify that the first stepper is blue initially and changes to green once Step 1 is completed", async ({
+    page,
+  }) => {});
 
-  test('Verify that personal input fields are enabled and accept user input', async ({ page }) => {
-
-  });
-
-  
+  test("Verify that personal input fields are enabled and accept user input", async ({
+    page,
+  }) => {});
 });
 
+/*
+<a _ngcontent-ng-c3758746842="" href="https://cydeo.com/terms-conditions/" target="_blank">Terms and conditions</a>
+*/
 
+//tageName[@attributeName='attributeValue']
+
+//a[@href='https://cydeo.com/terms-conditions/']
