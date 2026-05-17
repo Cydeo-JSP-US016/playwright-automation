0285b5101d3408234eedb58fa2276d22c55e10b2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2026-05-17 12:57:22 -0400

    Playwright Automation

--- tests/reviews/start-application-page.spec.js
+++ tests/reviews/start-application-page.spec.js
@@ -1,17 +1,11 @@
 import { test, expect } from "@playwright/test";
+import { CommonUI } from "./CommonUI";
 
 test.describe("Start Application Step", () => {
 
   test.beforeEach(async ({ page }) => {
-    let encodedCredentials = Buffer.from(
-      `${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`,
-    ).toString("base64"); // token ....
-
-    await page.setExtraHTTPHeaders({
-      Authorization: `Basic ${encodedCredentials}`,
-    });
-
-    await page.goto(process.env.SEP_QA_URL);
+   
+    await CommonUI.login(page);
     
   });
 
@@ -40,11 +34,16 @@ test.describe("Start Application Step", () => {
 
   test("Verify that the first stepper is blue initially and changes to green once Step 1 is completed", async ({
     page,
-  }) => {});
+  }) => {
+
+  });
 
   test("Verify that personal input fields are enabled and accept user input", async ({
     page,
-  }) => {});
+  }) => {
+    
+  });
+
 });
 
 /*
